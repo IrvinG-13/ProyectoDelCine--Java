efc5163ae987b967d2b44b6a588ceacddcc2e0b9
IrvinG-13 <irvigonza13@gmail.com>
2024-10-05 07:57:41 -0500

    first commit

---
+++ Tienda.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Productos{
+
+}
+
+class Carrito{
+    
+}
+
+public class Tienda{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n--Cafeteria del Cine Universitario--");
+        System.out.println("1.Combo1(Popcorn Mediano, Refresco Grande) ($4.50)");
+        System.out.println("2.Combo2(Hotdog, Refresco Grande) ($5.00)");
+        System.out.println("3.Combo3(Popcorn Grande, 2 Refrescos Medianos) ($6.80)");
+        System.out.println("4.Popcorn Chico ($1.25)");
+        System.out.println("5.Popcorn Mediano ($2.00)");
+        System.out.println("6.Popcorn Grande ($3.00)");
+        System.out.println("Si se desea acaramelado pagar ($0.50) adicional, igual en los combos");
+        System.out.println("7.Hotdog ($2.50)");
+        System.out.println("8.Chocolate ($1.75)");
+        System.out.println("9.Refrescos Pequeño ($1.30)");
+        System.out.println("10.Refrescos Mediano ($2.00)");
+        System.out.println("10.Refrescos Grande ($2.75)");
+        System.out.println("10.Agua ($1.50)");
+
+
+
+        sc.close();
+    }
+}
